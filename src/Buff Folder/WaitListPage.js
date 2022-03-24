81925235f4d3cd7467dd9404e4e0768a695eb3ab
nuelKaiser <majekodunmiimmanuel9@gmail.com>
2022-03-24 02:06:43 +0100

    built in modal

--- src/Buff Folder/WaitListPage.js
+++ src/Buff Folder/WaitListPage.js
@@ -3,21 +3,57 @@ import Footer from '../Resusable/Footer';
 import './WaitListPage.css';
 import { AiFillThunderbolt } from 'react-icons/ai';
 import emailjs from 'emailjs-com';
-
-function sendSubscribers(e) {
-  e.preventDefault();
-
-  emailjs.sendForm('service_pc18qbj', 'template_rdstg3a', e.target, 'gDwc1b1z9wwHYSl1p')
-    .then((result) => {
-      console.log(result.text);
-    }, (error) => {
-      console.log(error.text);
-    });
-
-  e.target.reset()
-}
+import { useState } from 'react';
+import { Dialog } from '@headlessui/react';
 
 function WaitListPage() {
+  let [isOpen, setIsOpen] = useState(false);
+  let [isNameInput, setIsNameInput] = useState('');
+  let [isEmailInput, setIsEmailInput] = useState('');
+  
+  
+
+  function sendSubscribers(e) {
+    e.preventDefault();
+    e.target.reset();
+    emailjs.sendForm('service_accpwyc', 'template_fu2zaed', e.target, 'ES_-cEuY2erXAFQRk')
+      .then((result) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      });
+      
+      console.log(e.target)
+      function reset() {
+        if (!isEmailInput || !isNameInput) {
+          setIsOpen(false)
+        } else {
+          setIsOpen(true)
+        }
+    
+        setIsEmailInput('')
+        setIsNameInput('')
+      }
+   
+
+      reset()
+
+     function disable(){
+        const  btn = document.getElementsByClassName('sub-btn');
+            if (!isEmailInput || !isNameInput) {
+              btn.disabled = true
+              btn.classList.add('no-btn')
+            } else {
+              btn.disabled = false
+              btn.classList.add('btn-btn')
+            }
+      }
+      disable()
+
+  }
+
+
+
   return (
     <div>
       <div className='access-container'>
@@ -40,14 +76,14 @@ function WaitListPage() {
             </div>
 
 
-            <div className='form-container'>
-              <form onSubmit={sendSubscribers}>
-                <input className='name-input' placeholder='First Name' type='text' name='name' />
-                <input className='email-input' placeholder='Email' type='email' name='email' />
+            <div className='form-container' >
+              <form onSubmit={sendSubscribers} name='form' >
+                <input className='name-input' placeholder='First Name' type='name' name='name'  value={isNameInput} onChange={(e) => setIsNameInput(e.target.value)} />
+                <input className='email-input' placeholder='Email' type='email' name='email'   value={isEmailInput} onChange={(e) => setIsEmailInput(e.target.value) }/>
 
 
                 <div className='submit-btn'>
-                  <button >
+                  <button className='sub-btn' >
                     Submit
                   </button>
                 </div>
@@ -55,6 +91,16 @@ function WaitListPage() {
             </div>
           </div>
         </div>
+
+        <Dialog open={isOpen} onClose={() => setIsOpen(false)} className='fixed'>
+          <Dialog.Overlay className={isOpen ? 'overlay-hidden' : 'overlay-show'} />
+          <Dialog.Title className='fixed-title'>Welcome Onboard</Dialog.Title>
+          <Dialog.Description className='fixed-words'>
+            Welcome on board to Buffer Africa! your email has been recieved. We'll reached out to shortly.
+          </Dialog.Description>
+          <button className='modal-btn' onClick={() => setIsOpen(false)}>Thank you</button>
+        </Dialog>
+
         <Footer />
       </div>
     </div>
